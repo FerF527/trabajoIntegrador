0ff7be04bdf1522a93838f66fdf523a6b7eec118
FerF527 <fernandofarias527@gmail.com>
2020-07-07 05:33:36 -0300

    agregado /autos/:marca con validacion de autos

--- controllers/autosController.js
+++ controllers/autosController.js
@@ -19,6 +19,40 @@ autos={
             })
         res.end();
     },
+     marcas: (req,res)=>{
+        res.set({'content-type':'text/plain;charset=utf-8'});
+        let autoSeleccionado = req.params.marca;
+         let autos = [];
+         let autoCorrecto = false;
+         dataConcesionarias.forEach(concesionaria=>{
+           concesionaria.autos.forEach(auto=>{
+               if(autos.indexOf(auto.marca) == -1){
+            autos.push(auto.marca)
+               }
+           });
+            });
+            autos.forEach(function(marca){
+                if(marca.toUpperCase().indexOf(autoSeleccionado.toUpperCase().trim()) !== -1){
+                    res.write('Bienvenido a continuación le detallamos los coches disponibles en base a su selección. \n\n');
+                    autoCorrecto = true;
+                }
+            }); 
+            
+            dataConcesionarias.forEach(function(sucursal){
+            sucursal.autos.forEach(function(auto){
+                if(auto.marca.toUpperCase() == autoSeleccionado.toUpperCase().trim()){
+                    res.write(` ■ Automovil: ${auto.marca} |■ Modelo: ${auto.modelo} |■ Año: ${auto.anio} |■ Color: ${auto.color}
+`);                 
+                }
+            })
+        })
+        if(autoCorrecto == false){
+        res.end('Por favor seleccione una marca disponible en nuestras sucursales');
+        }else{
+            res.end();
+        }
+    }
+     
     }
 
 
--- routes/autos.js
+++ routes/autos.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const autosController = require('../controllers/autosController')
 
 router.get('/', autosController.index);
-
+router.get('/:marca', autosController.marcas)
 module.exports = router;
